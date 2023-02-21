32a603a5f69fd6822f84a0a455f13f7815675e6a
Kalyan Annamalai <kalyan.shelf@gmail.com>
2023-02-21 16:02:56 +1100

    css module name

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,42 @@
-import { defineConfig } from 'vite';
+import { defineConfig, ConfigEnv } from 'vite';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-  publicDir: false,
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: 'src/index.ts',
-      name: 'MyLilButton',
-      fileName: 'my-button',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+export default (arg: ConfigEnv) => {
+  const generateScopedName =
+    arg.mode === 'production'
+      ? '[local]_[hash:base64:5]'
+      : '[name]_[local]_[hash:base64:5]';
+
+  return defineConfig({
+    plugins: [
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+    publicDir: false,
+    build: {
+      sourcemap: true,
+      lib: {
+        entry: 'src/index.ts',
+        name: 'MyLilButton',
+        fileName: 'my-button',
+        formats: ['es'],
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
         },
       },
     },
-  },
-});
+    css: {
+      modules: {
+        generateScopedName,
+      },
+    },
+  });
+};
